67d280884d692d9f545ffb135ee54f38749d217a
SidikR <rahmad.119140096@student.itera.ac.id>
2022-11-01 00:27:02 +0700

    menambahkan halaman register

--- resources/views/layout/main-auth.blade.php
+++ resources/views/layout/main-auth.blade.php
@@ -2,9 +2,12 @@
 <html lang="en">
     <head>
        @include('global.import')
+       <link rel="stylesheet" href="{{ asset('css/layout/main-auth.css') }}">
         <title>Document</title>
     </head>
     <body> 
-        @yield('rahmad')
+        <div class="main-wrapper w-100 vh-100 d-flex align-items-sm-start align-items-lg-center justify-content-center">
+            @yield('container')
+        </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
 Route::get('/', function () {
-    return view('auth.login');
+    return view('auth.register');
 });
